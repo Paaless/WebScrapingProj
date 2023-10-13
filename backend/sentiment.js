475f3cb50e4bc31015ea1fc145b1356ad05957b8
Paaless <45945412+Paaless@users.noreply.github.com>
2023-10-13 23:52:56 +0300

    Changed sentiment analysis library
    Changed library from "natural" to "vader-sentiment" as its better suitable for my case

--- backend/sentiment.js
+++ backend/sentiment.js
@@ -1,23 +1,19 @@
-import natural from "natural"
+import vader from 'vader-sentiment';
 
-const Analyzer = natural.SentimentAnalyzer;
-const stemmer = natural.PorterStemmer;
-const analyzer = new Analyzer("English",stemmer,"afinn");
+const interpretSentiment = (score) => {
+  if (score >= 4) return "Strongly Positive";
+  if (score > 0) return "Positive";
+  if (score === 0) return "Neutral";
+  if (score > -4) return "Negative";
+  return "Strongly Negative";
+};
 
-function interpretSentiment(score) {
-    if (score > 0.5) return "Strongly Positive";
-    if (score > 0) return "Positive";
-    if (score === 0) return "Neutral";
-    if (score > -0.5) return "Negative";
-    return "Strongly Negative";
-  }
+const parseSentimentText = (text) => {
+  const result = vader.SentimentIntensityAnalyzer.polarity_scores(text);
+  console.log("Result is : "+result.compound);
+  const humanReadable = interpretSentiment(result.compound);
+  console.log("Human readable result is: "+ humanReadable);
+  return humanReadable;
+};
 
-
-async function parseSentimentText(text){
-    console.log(text)
-    const result = analyzer.getSentiment(text.split(" "));
-    const humanReadable = interpretSentiment(result)
-    return humanReadable;
-}
-
-export {parseSentimentText}
+export { parseSentimentText };
